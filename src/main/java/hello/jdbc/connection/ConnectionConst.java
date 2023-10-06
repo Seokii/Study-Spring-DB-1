8d4a267940bb2b8a9f963dfbdb7f50a1d14f283f
Seokii <dev.seokii@gmail.com>
2023-10-06 17:01:09 +0900

    Section3: 트랜잭션-적용1

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,7 +1,9 @@
 package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    // public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String URL = "jdbc:h2:~/test"; // ubuntu에서 위의 접속 URL 에러가 발생해 이 코드로 대체함
+
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "";
 }
